b0b95e388ddc13bfe06f82a0829512527541be8e
vogelFritz <franciscosantin2002@gmail.com>
2025-09-11 15:37:57 -0300

    update agp version and other android configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.ttlock.ttlock_flutter"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "28.0.0000000"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.ttlock.ttlock_flutter"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation fileTree(dir: 'libs', include: ['*.jar'])
+   // implementation 'com.ttlock:ttlock:3.5.0'
+    implementation 'com.google.code.gson:gson:2.6.2'
+    implementation(name: 'ttlock-release', ext: 'aar')
+    implementation 'com.squareup.okhttp3:okhttp:4.12.0'
+}
